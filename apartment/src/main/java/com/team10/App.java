27972bddd9093f9086651c1bccbcefe6c9fc47e0
Andrew Miyaguchi <Andrew Miyaguchi>
2021-06-08 10:10:25 -0700

    Add header for the main app

--- apartment/src/main/java/com/team10/App.java
+++ apartment/src/main/java/com/team10/App.java
@@ -1,9 +1,16 @@
+/**
+ * UserInterface.java
+ * 
+ * Copyright (c) 2021
+ * Andrew Miyaguchi
+ * Eric Wang
+ * Steven Barberi
+ * California State University Long Beach
+ * 
+ */
+
 package com.team10;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
