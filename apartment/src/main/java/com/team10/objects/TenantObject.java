ae2fc6fe4d58e48830e891d8a950f21eaa89bfdf
Steven <stevent.barberi@gmail.com>
2021-06-21 01:58:25 -0700

    Setters and Getters

--- apartment/src/main/java/com/team10/objects/TenantObject.java
+++ apartment/src/main/java/com/team10/objects/TenantObject.java
@@ -18,4 +18,26 @@ public class TenantObject
     public TenantObject() {
         
     }
+
+    //Set and Get
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public String getName(){
+        return Name;
+    }
+
+    public void setAptNumber(int AptNumber){
+        this.AptNumber = AptNumber;
+    }
+    public int getAptNumber(){
+        return AptNumber;
+    }
+
+    public void setTenantID(int TenantID){
+        this.TenantID = TenantID;
+    }
+    public int getTanantID(){
+        return TanantID;
+    }
 }
