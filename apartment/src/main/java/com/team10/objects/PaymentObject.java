41be72edd4b0dd26c9ad2e923b1ab4aceec9e126
Steven <48588764+Steventbarberi@users.noreply.github.com>
2021-06-22 12:58:49 -0700

    Updated PaymentObject

--- apartment/src/main/java/com/team10/objects/PaymentObject.java
+++ apartment/src/main/java/com/team10/objects/PaymentObject.java
@@ -15,7 +15,19 @@ import com.team10.records.PaymentRecord;
 
 public class PaymentObject
 {
+    int TenantID;
+    int Month;
+    int Year;
+    Double PaymentAmount;
+
     public PaymentObject() {
         
+    }
+
+    public PaymentObject(int TenantID, int Month, int Year, Double PaymentAmount){
+        this.TenantID = TenantID;
+        this.Month = Month;
+        this.Year = Year;
+        this.PaymentAmount = PaymentAmount;
     }
 }
