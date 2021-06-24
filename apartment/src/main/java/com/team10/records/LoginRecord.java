85e7d42ef545595c32e3cf1fc5f865abe860db44
Steven <stevent.barberi@gmail.com>
2021-06-24 12:56:14 -0700

    Merge branch 'main' of https://github.com/XDleader555/CECS343 into main

--- apartment/src/main/java/com/team10/records/LoginRecord.java
+++ apartment/src/main/java/com/team10/records/LoginRecord.java
@@ -10,11 +10,28 @@
  */
 
 package com.team10.records;
+import java.util.ArrayList;
+
+import com.team10.objects.LoginObject;
 import com.team10.records.RecordParent;
 
 public class LoginRecord extends RecordParent
 {
+    Arraylist<LoginObject> LoginRecord;
+
     public LoginRecord() {
+        
+    }
+
+    public ArrayList getLoginRecord(){
+        return LoginRecord;
+    }
+
+    public void addLogin(LoginObject L){
+        LoginRecord.add(L);
+    }
+
+    public void displayLoginRecord(){
         
     }
 }
